0a64f7428eed95276b7d61fa9b5782dffa430bfa
Dan Ngo <danngo.au@gmail.com>
2024-06-27 23:20:43 +1000

    Signup v1.1

--- src/components/form/SignupForm.jsx
+++ src/components/form/SignupForm.jsx
@@ -1,57 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// import pastaImg from "../../assets/images/pasta.png";
-// main branch hasn't got the image yet
-
 const SignupForm = () => {
   return (
-    <article>
-      <section className="section1">
-          <div>
-            <h1>Welcome to Nom Nom</h1>
+    <form method="POST">
+      <p className="signin-field">
+        <label htmlFor="name">Full Name</label>
+        <br />
+        <input type="text" id="name" required="required" placeholder="John Doe" />
+      </p>
 
-            <form method="POST">
-              <p className="signin-field">
-                <label htmlFor="name">Full Name</label>
-                <br />
-                <input type="text" id="name" required="required" placeholder="John Doe" />
-              </p>
+      <p className="signin-field">
+        <label htmlFor="email">Email</label>
+        <br />
+        <input type="email" id="email" required="required" placeholder="Enter your Email here" />
+      </p>
 
-              <p className="signin-field">
-                <label htmlFor="email">Email</label>
-                <br />
-                <input type="email" id="email" required="required" placeholder="Enter your Email here" />
-              </p>
+      <p className="signin-field">
+        <label htmlFor="pwd">Password</label>
+        <br />
+        <input type="password" id="pwd" required="required" placeholder="Enter your Password" />
+      </p>
 
-              <p className="signin-field">
-                <label htmlFor="pwd">Password</label>
-                <br />
-                <input type="password" id="pwd" required="required" placeholder="Enter your Password" />
-              </p>
+      <p className="signin-field">
+        <label htmlFor="re-pwd">Confirm Password</label>
+        <br />
+        <input type="password" id="re-pwd" required="required" placeholder="Re-Enter your Password" />
+      </p>
 
-              <p className="signin-field">
-                <label htmlFor="re-pwd">Confirm Password</label>
-                <br />
-                <input type="password" id="re-pwd" required="required" placeholder="Re-Enter your Password" />
-              </p>
+      <input type="submit" value="Create Account" className="center" />
 
-              <input type="submit" value="Create Account" className="center" />
+      <p className="center">Already have an account? <Link to="">Login</Link></p>
 
-              <p className="center">Already have an account? <Link to="">Login</Link></p>
-
-              <p id="subscribe">
-                <input type="checkbox" id="news-letter"/>
-                <label htmlFor="news-letter">Subscribe to our News Letter</label>
-              </p>
-            </form>
-          </div>
-      </section>
-
-      <section className="section2">
-        <img /* src={pastaImg} */ alt="pasta" />
-      </section>
-    </article>
+      <p id="subscribe">
+        <input type="checkbox" id="news-letter"/>
+        <label htmlFor="news-letter">Subscribe to our News Letter</label>
+      </p>
+    </form>
   );
 };
 
--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import SignupForm from "../../components/form/SignupForm";
+
+import pastaImg from "../../assets/images/pasta.png";
+
 import "./Signin.css";
+import SignupForm from "../../components/form/SignupForm";
 
 const Signin = () => {
-  return <SignupForm />;
+  return (
+    <article className="signin-page">
+      <section className="section1">
+          <div>
+            <h1>Welcome to Nom Nom</h1>
+
+            <SignupForm />
+          </div>
+      </section>
+
+      <section className="section2">
+        <img src={pastaImg} alt="pasta" />
+      </section>
+    </article>
+  );
 };
 
 export default Signin;
